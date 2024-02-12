5830636a1dcc1462aa60e7f2ce63f61be8fb47d4
Maajoo <mio.kantola@hotmail.com>
2024-02-12 12:39:51 +0200

    thymeleaf lisätty

--- bookstore/src/main/java/bkstr/bookstore/web/BookController.java
+++ bookstore/src/main/java/bkstr/bookstore/web/BookController.java
@@ -2,13 +2,11 @@ package bkstr.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class BookController {
     @RequestMapping("/index")
     public String giveEtusivu(){
-        return "Welcome to the bookstore!";
+        return "Bookstore";
     }
 }
