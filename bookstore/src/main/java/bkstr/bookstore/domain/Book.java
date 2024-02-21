50ae01808a37165475647c753b96a185bbd027c0
Maajoo <mio.kantola@hotmail.com>
2024-02-21 12:51:50 +0200

    @Entity Book.java

--- bookstore/src/main/java/bkstr/bookstore/domain/Book.java
+++ bookstore/src/main/java/bkstr/bookstore/domain/Book.java
@@ -1,11 +1,68 @@
 package bkstr.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String title;
     private String author;
     private Integer publicationYear;
     private Integer isbn;
     private Double price;
+
+    public Book(Long id, String title, String author, Integer publicationYear, Integer isbn, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public Integer getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    
 
 }
