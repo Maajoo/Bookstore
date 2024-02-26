e282c039f7b9c7c028dc4ebc931b3c715c8f1496
Maajoo <mio.kantola@hotmail.com>
2024-02-26 19:56:39 +0200

    C3.3

--- bookstore/src/main/java/bkstr/bookstore/web/BookController.java
+++ bookstore/src/main/java/bkstr/bookstore/web/BookController.java
@@ -3,8 +3,11 @@ package bkstr.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import bkstr.bookstore.domain.Book;
 import bkstr.bookstore.domain.BookRepository;
 
 @Controller
@@ -18,6 +21,29 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+
+
+    //DELETE
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+
+    //ADD
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    //SAVE
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
 
     @RequestMapping("/index")
     public String giveEtusivu() {
