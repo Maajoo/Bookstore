11d9b32432d5e43541193bcac15cbb0f3a697d49
Maajoo <mio.kantola@hotmail.com>
2024-02-26 19:14:58 +0200

    c3.2

--- bookstore/src/main/java/bkstr/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/bkstr/bookstore/BookstoreApplication.java
@@ -19,8 +19,8 @@ public class BookstoreApplication {
 	public CommandLineRunner demo(BookRepository repository) {
 		return (args) -> {
 			// Your code...add some demo data to db
-			Book book1 = new Book("Muumipappa ja meri", "Tove Jansson", "1965", "9780374350321", "15,95");
-			Book book2 = new Book("Kapteeni Sinikarhun 13 1/2 elämää", "Walter Moers ", "2006", "9783813505726", "25,00");
+			Book book1 = new Book("Muumipappa ja meri", "Tove Jansson", "1965", "9780374350321", "15,95€");
+			Book book2 = new Book("Kapteeni Sinikarhun 13 1/2 elämää", "Walter Moers ", "2006", "9783813505726", "25,00€");
 			repository.save(book1);
 			repository.save(book2);
 
--- bookstore/src/main/java/bkstr/bookstore/web/BookController.java
+++ bookstore/src/main/java/bkstr/bookstore/web/BookController.java
@@ -1,12 +1,26 @@
 package bkstr.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import bkstr.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookRepository repository;
+
+    @RequestMapping("/booklist")
+    public String giveBooklist(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+
     @RequestMapping("/index")
-    public String giveEtusivu(){
+    public String giveEtusivu() {
         return "Bookstore";
     }
 }
