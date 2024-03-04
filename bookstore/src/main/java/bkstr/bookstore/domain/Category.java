5ac8ae5b859ad5ad292d2eff01c08286be73b1db
Maajoo <mio.kantola@hotmail.com>
2024-03-04 16:21:09 +0200

    C5

--- bookstore/src/main/java/bkstr/bookstore/domain/Category.java
+++ bookstore/src/main/java/bkstr/bookstore/domain/Category.java
@@ -2,6 +2,8 @@ package bkstr.bookstore.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +18,7 @@ public class Category {
     private Long categoryid;
     private String name;
 
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private List<Book> books;
 
--- bookstore/src/main/java/bkstr/bookstore/web/BookController.java
+++ bookstore/src/main/java/bkstr/bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package bkstr.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import bkstr.bookstore.domain.Book;
 import bkstr.bookstore.domain.BookRepository;
@@ -63,6 +67,17 @@ public class BookController {
         return "editbook";
     }
 
+    //RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+        return (List<Book>) repository.findAll();
+    }
+
+    //RESTful service to get a book by id
+    @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+        return repository.findById(bookId);
+    }
 
     // ETUSIVU
     @RequestMapping("/index")
