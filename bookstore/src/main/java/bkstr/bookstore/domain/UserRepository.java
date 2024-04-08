5070d5f990482dcb67c9e7b467c1875cc55eef34
Maajoo <mio.kantola@hotmail.com>
2024-04-08 12:17:05 +0300

    kirjautuminen korjattu

--- bookstore/src/main/java/bkstr/bookstore/domain/UserRepository.java
+++ bookstore/src/main/java/bkstr/bookstore/domain/UserRepository.java
@@ -1,10 +1,6 @@
 package bkstr.bookstore.domain;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    static User findByUsername(String username) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findByUsername'");
-    }
+    User findByUsername(String username);
 }
--- bookstore/src/main/java/bkstr/bookstore/web/UserDetailServiceImpl.java
+++ bookstore/src/main/java/bkstr/bookstore/web/UserDetailServiceImpl.java
@@ -26,7 +26,7 @@ public class UserDetailServiceImpl implements UserDetailsService  {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
     {   
-    	User curruser = UserRepository.findByUsername(username);
+    	User curruser = repository.findByUsername(username);
         UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
         		AuthorityUtils.createAuthorityList(curruser.getRole()));
         return user;
